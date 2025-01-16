926270752de091d0270733cfc4618d47c353d592
Tirth S Pandya <teamfznltd@gmail.com>
2025-01-16 14:39:13 +0530

    ,.

---
+++ navbar/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <h1>Welcome to My Website</h1>
+        {/* Add more content here */}
+      </main>
+    </div>
+  );
+};
+
+export default App;
